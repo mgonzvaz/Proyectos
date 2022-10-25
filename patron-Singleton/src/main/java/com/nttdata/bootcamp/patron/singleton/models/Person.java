949c8a88120ddcfeb9e1d6ba7b1ef43f4f52fd9f
mgonzvaz <116562803+mgonzvaz@users.noreply.github.com>
2022-10-25 10:38:43 +0200

    Add files via upload

---
+++ patron-Singleton/src/main/java/com/nttdata/bootcamp/patron/singleton/models/Person.java
@@ -0,0 +1,14 @@
+package com.nttdata.bootcamp.patron.singleton.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+	private String name;
+	private int age;
+
+}
